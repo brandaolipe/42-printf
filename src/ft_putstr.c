5b90283ff1560125c59f92b02338b59eaa2ceb08
febranda <felipebrandaochaves@gmail.com>
2025-08-27 20:00:47 -0300

    feat: ft_putstr()

---
+++ src/ft_putstr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 16:55:12 by febranda          #+#    #+#             */
+/*   Updated: 2025/08/27 17:04:34 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putstr(char *str)
+{
+	int	count;
+
+	if (!str)
+		return (ft_putstr("(null)"));
+	count = 0;
+	while (*str)
+		count += ft_putchar(*str++);
+	return (count);
+}
