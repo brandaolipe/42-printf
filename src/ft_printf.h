40d5d3d9c8d0d287589ff2304d27b30adf9787bf
febranda <felipebrandaochaves@gmail.com>
2025-08-27 20:03:29 -0300

    .

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,16 +6,20 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:49 by febranda          #+#    #+#             */
-/*   Updated: 2025/08/22 19:08:06 by febranda         ###   ########.fr       */
+/*   Updated: 2025/08/27 19:56:13 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include	<stdarg.h>
 # include	<stddef.h>
 # include	<unistd.h>
 
 int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int nb);
 
 #endif
