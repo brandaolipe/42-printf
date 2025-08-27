940bae4aedd20e385d925a95644f79e256c4ecdf
febranda <felipebrandaochaves@gmail.com>
2025-08-27 20:03:11 -0300

    working with: %c, %s, %d, %i

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,41 +6,62 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 15:26:37 by febranda          #+#    #+#             */
-/*   Updated: 2025/08/22 19:10:37 by febranda         ###   ########.fr       */
+/*   Updated: 2025/08/27 19:55:19 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar(char c);
+int	ft_putnbr(int nb);
 
-void	ft_handle_specifier(char c)
+int	ft_handle_specifier(char specifier, va_list ap)
 {
+	int	count;
+
+	count = 0;
+	if (specifier == 'c')
+		count += ft_putchar(va_arg(ap, int));
+	if (specifier == 's')
+		count += ft_putstr(va_arg(ap, char *));
+	if (specifier == 'd' || specifier == 'i')
+		count += ft_putnbr(va_arg(ap, int));
+//	if (specifier == 'u')
+//		ft_putnbr();
+//	if (specifier == 'x')
+//		ft_putnbr();
+//	if (specifier == 'X')
+//		ft_putnbr();
+	return (count);
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *format, ...)
 {
-	size_t	len;
-	size_t	i;
+	va_list	ap;
+	size_t	count;
 
-	len = 0;
-	i = 0;
-	while (str[i])
+	va_start(ap, format);
+	count = 0;
+	while (*format)
 	{
-		ft_putchar(str[i]);
-		len++;
-		i++;
+		if (*format == '%')
+		{
+			format++;
+			count += ft_handle_specifier(*format, ap);
+			format++;
+		}
+		else
+		{
+			count += write(1, format, 1);
+			format++;
+		}
 	}
-	return (len);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
+	return (count);
 }
 
 // int	main(void)
 // {
-// 	ft_printf("oi");
-// 	return (0);
-// }
+// 	int	number;
+// 	number = 10;
+//  	ft_printf("%d", number);
+//  	return (0);
+// }	
