4d24f4bde5ab39bd4aeadaaee6fd9be94a9b36ed
febranda <felipebrandaochaves@gmail.com>
2025-08-28 20:51:04 -0300

    feat ft_puthex()

---
+++ src/ft_puthex.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 16:47:08 by febranda          #+#    #+#             */
+/*   Updated: 2025/08/28 20:47:44 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_puthex(unsigned int nb, char specifier)
+{
+	int	count;
+	char	*basehex;
+
+	count = 0;
+	if (specifier == 'X')
+		basehex = "0123456789ABCDEF";
+	else
+		basehex = "0123456789abcdef";
+	if (nb < 16)
+	{
+		count += ft_putchar(basehex[nb]);
+	}
+	else
+	{
+		count += ft_puthex(nb / 16, specifier);
+		count += ft_puthex(nb % 16, specifier);
+	}
+	return (count);
+}
+
+//012345
