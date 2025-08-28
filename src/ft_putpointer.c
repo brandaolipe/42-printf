49ed6f5fe73fd807734beaeaaaa37d397ddb32e8
febranda <felipebrandaochaves@gmail.com>
2025-08-28 20:51:23 -0300

    feat ft_putpointer()

---
+++ src/ft_putpointer.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putpointer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 19:46:22 by febranda          #+#    #+#             */
+/*   Updated: 2025/08/28 20:44:49 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putpointer(unsigned long nb)
+{
+	int count;
+
+	count = 0;
+	if (!nb)
+	{
+		write (1, "(nil)", 5);
+		return (5);
+	}
+	count += ft_putstr("0x");
+	count += ft_puthex(nb, 'x');
+	return (count);
+}
