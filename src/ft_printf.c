4f70cf9c3a6d9cbaab3bbb1b9cde5385002ab239
febranda <felipebrandaochaves@gmail.com>
2025-08-30 13:03:05 -0300

    feat: ft_printf()

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,13 +6,11 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 15:26:37 by febranda          #+#    #+#             */
-/*   Updated: 2025/08/27 19:55:19 by febranda         ###   ########.fr       */
+/*   Updated: 2025/08/29 17:39:36 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	ft_putnbr(int nb);
 
 int	ft_handle_specifier(char specifier, va_list ap)
 {
@@ -25,12 +23,16 @@ int	ft_handle_specifier(char specifier, va_list ap)
 		count += ft_putstr(va_arg(ap, char *));
 	if (specifier == 'd' || specifier == 'i')
 		count += ft_putnbr(va_arg(ap, int));
-//	if (specifier == 'u')
-//		ft_putnbr();
-//	if (specifier == 'x')
-//		ft_putnbr();
-//	if (specifier == 'X')
-//		ft_putnbr();
+	if (specifier == 'u')
+		count += ft_putunsig(va_arg(ap, unsigned int));
+	if (specifier == 'x')
+		count += ft_puthex(va_arg(ap, unsigned int), specifier);
+	if (specifier == 'X')
+		count += ft_puthex(va_arg(ap, unsigned int), specifier);
+	if (specifier == 'p')
+		count += ft_putpointer(va_arg(ap, unsigned long int));
+	if (specifier == '%')
+		count += ft_putchar('%');
 	return (count);
 }
 
@@ -55,13 +57,6 @@ int	ft_printf(const char *format, ...)
 			format++;
 		}
 	}
+	va_end(ap);
 	return (count);
 }
-
-// int	main(void)
-// {
-// 	int	number;
-// 	number = 10;
-//  	ft_printf("%d", number);
-//  	return (0);
-// }	
