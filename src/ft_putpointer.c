897caf7d3ebb94d205741fa9c30113787ba06a44
febranda <felipebrandaochaves@gmail.com>
2025-08-30 13:02:22 -0300

    fix: now works with unsigned long

--- src/ft_putpointer.c
+++ src/ft_putpointer.c
@@ -6,15 +6,15 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 19:46:22 by febranda          #+#    #+#             */
-/*   Updated: 2025/08/28 20:44:49 by febranda         ###   ########.fr       */
+/*   Updated: 2025/08/29 17:20:08 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putpointer(unsigned long nb)
+int	ft_putpointer(unsigned long int nb)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	if (!nb)
