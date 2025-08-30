280da55c29771c72dc12df07fbeceb784bf89f19
febranda <felipebrandaochaves@gmail.com>
2025-08-30 13:04:41 -0300

    .

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:49 by febranda          #+#    #+#             */
-/*   Updated: 2025/08/27 19:56:13 by febranda         ###   ########.fr       */
+/*   Updated: 2025/08/29 17:40:32 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define FT_PRINTF_H
 
 # include	<stdarg.h>
-# include	<stddef.h>
 # include	<unistd.h>
 
 int	ft_printf(const char *str, ...);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int nb);
+int	ft_putunsig(unsigned int nb);
+int	ft_puthex(unsigned long int nb, char specifier);
+int	ft_putpointer(unsigned long int nb);
 
 #endif
